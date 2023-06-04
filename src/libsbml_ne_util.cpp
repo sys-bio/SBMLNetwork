9d3ad8fc94bc1ba76a86d4ca5c2bf0f7620055b8
adelhpour <>
2023-06-04 14:45:07 -0700

    function comments are updated adn some new functions are aded

--- src/libsbml_ne_util.cpp
+++ src/libsbml_ne_util.cpp
@@ -1,6 +1,7 @@
 #include "libsbml_ne_util.h"
 #include "libsbml_ne_layout.h"
 #include "libsbml_ne_render.h"
+#include "libsbml_ne_util_helpers.h"
 
 namespace LIBSBML_NETWORKEDITOR_CPP_NAMESPACE  {
 
@@ -23,42 +24,86 @@ const std::string writeSBML(SBMLDocument* document) {
     return writeSBMLToString(document);
 }
 
-int addDefaultLayout(SBMLDocument* document) {
+int createDefaultLayout(SBMLDocument* document) {
     if (!getNumLayouts(document)) {
         Layout* layout = createLayout(document);
-        setDefaultLayoutFeatures(document, layout);
-        return addLayout(document, layout);
+        return setDefaultLayoutFeatures(document, layout);
     }
 
     return -1;
 }
 
-int addDefaultRenderInformation(SBMLDocument* document) {
-    if (!addDefaultLocalRenderInformation(document) && !addDefaultGlobalRenderInformation(document))
+int createDefaultRenderInformation(SBMLDocument* document) {
+    if (!createDefaultLocalRenderInformation(document) && !createDefaultGlobalRenderInformation(document))
         return 0;
 
     return -1;
 }
 
-int addDefaultGlobalRenderInformation(SBMLDocument* document) {
+int createDefaultGlobalRenderInformation(SBMLDocument* document) {
     if (!getNumGlobalRenderInformation(document)) {
         GlobalRenderInformation* globalRenderInformation = createGlobalRenderInformation(document);
-        setDefaultGlobalRenderInformationFeatures(document, globalRenderInformation);
-        return addGlobalRenderInformation(document, globalRenderInformation);
+        return setDefaultGlobalRenderInformationFeatures(document, globalRenderInformation);
     }
 
     return -1;
 }
 
-int addDefaultLocalRenderInformation(SBMLDocument* document) {
+int createDefaultLocalRenderInformation(SBMLDocument* document) {
     Layout* layout = getLayout(document);
     if (!getNumLocalRenderInformation(layout)) {
         LocalRenderInformation* localRenderInformation = createLocalRenderInformation(layout);
-        setDefaultLocalRenderInformationFeatures(document,layout, localRenderInformation);
-        return addLocalRenderInformation(layout, localRenderInformation);
+        return setDefaultLocalRenderInformationFeatures(document,layout, localRenderInformation);
     }
 
     return -1;
+}
+
+ListOfGraphicalObjects getListOfGraphicalObjects(SBMLDocument* document, const std::string& id) {
+    ListOfGraphicalObjects listOfGraphicalObjects;
+    mergeListsOfGraphicalObjects(listOfGraphicalObjects, getListOfCompartmentGlyphs(document, id));
+    mergeListsOfGraphicalObjects(listOfGraphicalObjects, getListOfSpeciesGlyphs(document, id));
+    mergeListsOfGraphicalObjects(listOfGraphicalObjects, getListOfReactionGlyphs(document, id));
+    return listOfGraphicalObjects;
+}
+
+ListOfGraphicalObjects getListOfCompartmentGlyphs(SBMLDocument* document, const std::string& id) {
+    ListOfGraphicalObjects listOfCompartmentGlyphs;
+    Layout* layout = getLayout(document);
+    if (layout) {
+        for (unsigned int i = 0; i < layout->getNumCompartmentGlyphs(); i++) {
+            if (layout->getCompartmentGlyph(i)->getCompartmentId() == id)
+                listOfCompartmentGlyphs.append(layout->getCompartmentGlyph(i));
+        }
+    }
+
+    return listOfCompartmentGlyphs;
+}
+
+ListOfGraphicalObjects getListOfSpeciesGlyphs(SBMLDocument* document, const std::string& id) {
+    ListOfGraphicalObjects listOfSpeciesGlyphs;
+    Layout* layout = getLayout(document);
+    if (layout) {
+        for (unsigned int i = 0; i < layout->getNumSpeciesGlyphs(); i++) {
+            if (layout->getSpeciesGlyph(i)->getSpeciesId() == id)
+                listOfSpeciesGlyphs.append(layout->getSpeciesGlyph(i));
+        }
+    }
+
+    return listOfSpeciesGlyphs;
+}
+
+ListOfGraphicalObjects getListOfReactionGlyphs(SBMLDocument* document, const std::string& id) {
+    ListOfGraphicalObjects listOfReactionGlyphs;
+    Layout* layout = getLayout(document);
+    if (layout) {
+        for (unsigned int i = 0; i < layout->getNumReactionGlyphs(); i++) {
+            if (layout->getReactionGlyph(i)->getReactionId() == id)
+                listOfReactionGlyphs.append(layout->getReactionGlyph(i));
+        }
+    }
+
+    return listOfReactionGlyphs;
 }
 
 }
--- src/libsbml_ne_util.h
+++ src/libsbml_ne_util.h
@@ -1,5 +1,5 @@
-#ifndef __LIBSBML_NE_SBML_H_
-#define __LIBSBML_NE_SBML_H_
+#ifndef __LIBSBML_NE_UTIL_H_
+#define __LIBSBML_NE_UTIL_H_
 
 #include "libsbml_ne_common.h"
 
@@ -11,40 +11,71 @@
 
 namespace LIBSBML_NETWORKEDITOR_CPP_NAMESPACE {
 
-/// @brief Get the Layout of the SBML document.
-/// @param document a pointer to the SBMLDocument object.
-/// @return a pointer to the Layout object of the SBML document.
+/// @brief Reads an SBML document from the given file name or the given text string
+/// @param sbml either the name or full pathname of the file to be read or a string containing a full SBML model.
+/// @return a pointer to the SBMLDocument structure created from the SBML content in the given file name or
+/// from the SBML content in the given text string.
 LIBSBML_NETWORKEDITOR_EXTERN SBMLDocument* readSBML(const std::string &sbml);
 
-/// @brief Get the Layout of the SBML document.
-/// @param document a pointer to the SBMLDocument object.
-/// @return a pointer to the Layout object of the SBML document.
+/// @brief Writes the given SBML document to a filename.
+/// @param document the SBML document to be written.
+/// @param fileName the name or full pathname of the file where the SBML is to be written.
+/// @return true on success and false if the filename could not be opened for writing.
 LIBSBML_NETWORKEDITOR_EXTERN bool writeSBML(SBMLDocument* document, const std::string &fileName);
 
-/// @brief Get the Layout of the SBML document.
-/// @param document a pointer to the SBMLDocument object.
-/// @return a pointer to the Layout object of the SBML document.
+/// @brief Writes the given SBML document to a string and returns it.
+/// @param document the SBML document to be written.
+/// @return the string on success and empty string if one of the underlying parser components fail.
 LIBSBML_NETWORKEDITOR_EXTERN const std::string writeSBML(SBMLDocument* document);
 
-/// @brief Get the Layout of the SBML document.
+/// @brief Create a Layout object, add it to list of layouts of the SBML document, and
+/// set all the necessary features for it
 /// @param document a pointer to the SBMLDocument object.
-/// @return a pointer to the Layout object of the SBML document.
-LIBSBML_NETWORKEDITOR_EXTERN int addDefaultLayout(SBMLDocument* document);
+/// @return integer value indicating success/failure of the function.
+LIBSBML_NETWORKEDITOR_EXTERN int createDefaultLayout(SBMLDocument* document);
 
-/// @brief Get the Layout of the SBML document.
+/// @brief Create a GlobalRenderInformation object and add it to list of global renders of the SBML document, and
+/// Create a LocalRenderInformation object, add it to list of local renders of the Layout of the SBML document, and
+/// set all the necessary features for them.
 /// @param document a pointer to the SBMLDocument object.
-/// @return a pointer to the Layout object of the SBML document.
-LIBSBML_NETWORKEDITOR_EXTERN int addDefaultRenderInformation(SBMLDocument* document);
+/// @return integer value indicating success/failure of the function.
+LIBSBML_NETWORKEDITOR_EXTERN int createDefaultRenderInformation(SBMLDocument* document);
 
-/// @brief Get the Layout of the SBML document.
+/// @brief Create a GlobalRenderInformation object, add it to list of global renders of the SBML document, and
+/// set all the necessary features for it.
 /// @param document a pointer to the SBMLDocument object.
-/// @return a pointer to the Layout object of the SBML document.
-LIBSBML_NETWORKEDITOR_EXTERN int addDefaultGlobalRenderInformation(SBMLDocument* document);
+/// @return integer value indicating success/failure of the function.
+LIBSBML_NETWORKEDITOR_EXTERN int createDefaultGlobalRenderInformation(SBMLDocument* document);
 
-/// @brief Get the Layout of the SBML document.
+/// @brief Create a LocalRenderInformation object, add it to list of local renders of the Layout of the SBML document, and
+/// set all the necessary features for it.
 /// @param document a pointer to the SBMLDocument object.
-/// @return a pointer to the Layout object of the SBML document.
-LIBSBML_NETWORKEDITOR_EXTERN int addDefaultLocalRenderInformation(SBMLDocument* document);
+/// @return integer value indicating success/failure of the function.
+LIBSBML_NETWORKEDITOR_EXTERN int createDefaultLocalRenderInformation(SBMLDocument* document);
+
+/// @brief Returns a list of GraphicalObject objects associated with the entered model entity id.
+/// @param document a pointer to the SBMLDocument object.
+/// @param id the id of the model entity to the GraphicalObject objects of which are going to be returned.
+/// @return a pointer to the GraphicalObject object with the same graphical object id.
+LIBSBML_NETWORKEDITOR_EXTERN ListOfGraphicalObjects getListOfGraphicalObjects(SBMLDocument* document, const std::string& id);
+
+/// @brief Returns a list of GraphicalObject objects associated with the entered model entity id.
+/// @param document a pointer to the SBMLDocument object.
+/// @param id the id of the model entity to the GraphicalObject objects of which are going to be returned.
+/// @return a pointer to the GraphicalObject object with the same graphical object id.
+LIBSBML_NETWORKEDITOR_EXTERN ListOfGraphicalObjects getListOfCompartmentGlyphs(SBMLDocument* document, const std::string& id);
+
+/// @brief Returns a list of GraphicalObject objects associated with the entered model entity id.
+/// @param document a pointer to the SBMLDocument object.
+/// @param id the id of the model entity to the GraphicalObject objects of which are going to be returned.
+/// @return a pointer to the GraphicalObject object with the same graphical object id.
+LIBSBML_NETWORKEDITOR_EXTERN ListOfGraphicalObjects getListOfSpeciesGlyphs(SBMLDocument* document, const std::string& id);
+
+/// @brief Returns a list of GraphicalObject objects associated with the entered model entity id.
+/// @param document a pointer to the SBMLDocument object.
+/// @param id the id of the model entity to the GraphicalObject objects of which are going to be returned.
+/// @return a pointer to the GraphicalObject object with the same graphical object id.
+LIBSBML_NETWORKEDITOR_EXTERN ListOfGraphicalObjects getListOfReactionGlyphs(SBMLDocument* document, const std::string& id);
 
 }
 
