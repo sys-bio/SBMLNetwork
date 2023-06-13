783795aa9730850f17ac4fd92644b82e2eec0959
adelhpour <>
2023-06-13 13:14:27 -0700

    util layout source and header files are added so that we can create another layer for layout functions

---
+++ src/libsbml_ne_util_layout.cpp
@@ -0,0 +1,7 @@
+#include "libsbml_ne_util_layout.h"
+#include "libsbml_ne_layout.h"
+#include "libsbml_ne_layout_helpers.h"
+
+namespace LIBSBML_NETWORKEDITOR_CPP_NAMESPACE  {
+
+}
---
+++ src/libsbml_ne_util_layout.h
@@ -0,0 +1,15 @@
+#ifndef __LIBSBML_NE_UTIL_LAYOUT_H_
+#define __LIBSBML_NE_UTIL_LAYOUT_H_
+
+#include "libsbml_ne_common.h"
+
+#ifndef SWIG
+#include "sbml/SBMLTypes.h"
+#include "sbml/packages/layout/common/LayoutExtensionTypes.h"
+#endif
+
+namespace LIBSBML_NETWORKEDITOR_CPP_NAMESPACE {
+
+}
+
+#endif
