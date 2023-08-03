0f3df44b5c5fa271aee7c6bfb9f197d8e6f3da0d
adelhpour <>
2023-08-02 23:47:13 -0700

    bug fixes for defining LIBSBML_NETWORKEDITOR_EXTERN

--- src/libsbml_ne_common.h
+++ src/libsbml_ne_common.h
@@ -1,13 +1,14 @@
 #ifndef __LIBSBML_NE_COMMON_H_
 #define __LIBSBML_NE_COMMON_H_
 
-#define LIBSBML_NETWORKEDITOR_EXTERN
 # if defined (_WIN32)
     #if(LIBSBML_NETWORKEDITOR_EXPORTS)
         #define LIBSBML_NETWORKEDITOR_EXTERN __declspec(dllexport)
     #else
         #define LIBSBML_NETWORKEDITOR_EXTERN __declspec(dllimport)
     #endif
+# else
+#define LIBSBML_NETWORKEDITOR_EXTERN
 #endif
 
 #include <iostream>
