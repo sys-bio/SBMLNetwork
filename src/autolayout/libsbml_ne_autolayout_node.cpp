0d2f9afb8ba6e870f52dd62d0ed0dc6055752e30
adelhpour <>
2023-10-23 12:37:10 -0700

    node member functions are added

--- src/autolayout/libsbml_ne_autolayout_node.cpp
+++ src/autolayout/libsbml_ne_autolayout_node.cpp
@@ -1,5 +1,66 @@
 #include "libsbml_ne_autolayout_node.h"
 
-AutoLayoutNode::AutoLayoutNode() {
+AutoLayoutNode::AutoLayoutNode(GraphicalObject* graphicalObject) {
+    _graphicalObject = graphicalObject;
+    _degree = 0;
+}
 
+const std::string AutoLayoutNode::getId() {
+    return _graphicalObject->getId();
+}
+
+const double AutoLayoutNode::getX() {
+    return _graphicalObject->getBoundingBox()->x();
+};
+
+void AutoLayoutNode::setX(const double& x) {
+    _graphicalObject->getBoundingBox()->setX(x);
+}
+
+const double AutoLayoutNode::getY() {
+    return _graphicalObject->getBoundingBox()->y();
+};
+
+void AutoLayoutNode::setY(const double& y) {
+    _graphicalObject->getBoundingBox()->setY(y);
+}
+
+const double AutoLayoutNode::getWidth() {
+    return _graphicalObject->getBoundingBox()->width();
+};
+
+void AutoLayoutNode::setWidth(const double& width) {
+    _graphicalObject->getBoundingBox()->setWidth(width);
+}
+
+const double AutoLayoutNode::getHeight() {
+    return _graphicalObject->getBoundingBox()->height();
+};
+
+void AutoLayoutNode::setHeight(const double& height) {
+    _graphicalObject->getBoundingBox()->setHeight(height);
+}
+
+const double AutoLayoutNode::getDisplacementX() {
+    return _displacementX;
+}
+
+void AutoLayoutNode::setDisplacementX(const double& dx) {
+    _displacementX = dx;
+}
+
+const double AutoLayoutNode::getDisplacementY() {
+    return _displacementY;
+}
+
+void AutoLayoutNode::setDisplacementY(const double& dy) {
+    _displacementY= dy;
+}
+
+const int AutoLayoutNode::getDegree() {
+    return _degree;
+}
+
+void AutoLayoutNode::setDegree(const int& degree) {
+    _degree = degree;
 }
--- src/autolayout/libsbml_ne_autolayout_node.h
+++ src/autolayout/libsbml_ne_autolayout_node.h
@@ -1,10 +1,52 @@
 #ifndef __LIBSBML_NE_AUTOLAYOUT_NODE__H
 #define __LIBSBML_NE_AUTOLAYOUT_NODE__H
 
+#ifndef SWIG
+#include "sbml/SBMLTypes.h"
+#include "sbml/packages/layout/common/LayoutExtensionTypes.h"
+#endif
+
 class AutoLayoutNode {
 public:
 
-    AutoLayoutNode();
+    AutoLayoutNode(GraphicalObject* graphicalObject);
+
+    const std::string getId();
+
+    const double getX();
+
+    void setX(const double& x);
+
+    const double getY();
+
+    void setY(const double& y);
+
+    const double getWidth();
+
+    void setWidth(const double& width);
+
+    const double getHeight();
+
+    void setHeight(const double& height);
+
+    const double getDisplacementX();
+
+    void setDisplacementX(const double& dx);
+
+    const double getDisplacementY();
+
+    void setDisplacementY(const double& dy);
+
+    const int getDegree();
+
+    void setDegree(const int& degree);
+
+protected:
+
+    GraphicalObject* _graphicalObject;
+    double _displacementX;
+    double _displacementY;
+    int _degree;
 };
 
 #endif
