625147504a9a899fd0826a164ac459dd8d1ef951
adelhpour <>
2023-10-30 17:18:04 -0700

    - set and get position methods are added to the auto layout point class
    - the method to check if a point is empty (0.0, 0.0) is added to the auto layout point class

--- src/autolayout/libsbml_ne_autolayout_point.cpp
+++ src/autolayout/libsbml_ne_autolayout_point.cpp
@@ -1,11 +1,13 @@
 #include "libsbml_ne_autolayout_point.h"
 
+#include "iostream"
+
 AutoLayoutPoint::AutoLayoutPoint(const double& x, const double& y) {
     _x = x;
     _y = y;
 }
 
-const double AutoLayoutPoint::getX() {
+const double& AutoLayoutPoint::getX() const {
     return _x;
 };
 
@@ -13,10 +15,26 @@ void AutoLayoutPoint::setX(const double& x) {
     _x = x;
 }
 
-const double AutoLayoutPoint::getY() {
+const double& AutoLayoutPoint::getY() const {
     return _y;
 };
 
 void AutoLayoutPoint::setY(const double& y) {
     _y = y;
+}
+
+void AutoLayoutPoint::setPosition(const AutoLayoutPoint position) {
+    _x = position.getX();
+    _y = position.getY();
+}
+
+const AutoLayoutPoint AutoLayoutPoint::getPosition() {
+    return AutoLayoutPoint(_x, _y);
+}
+
+const bool AutoLayoutPoint::isEmpty() {
+    if (std::abs(_x) > 0.000001 || std::abs(_y) > 0.000001)
+        return true;
+
+    return false;
 }
--- src/autolayout/libsbml_ne_autolayout_point.h
+++ src/autolayout/libsbml_ne_autolayout_point.h
@@ -6,13 +6,19 @@ public:
 
     AutoLayoutPoint(const double& x = 0.0, const double& y = 0.0);
 
-    const double getX();
+    const double& getX() const;
 
     void setX(const double& x);
 
-    const double getY();
+    const double& getY() const;
 
     void setY(const double& y);
+
+    void setPosition(const AutoLayoutPoint position);
+
+    const AutoLayoutPoint getPosition();
+
+    const bool isEmpty();
 
 protected:
 
