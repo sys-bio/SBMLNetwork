047435724042584e444fc1e15d66cbdeb7e4bb2b
adelhpour <ad.heydarabadi@gmail.com>
2023-11-17 13:00:45 -0800

    M_PI is defined in case it is not included in the cmath library (which is the case on Windows)

--- src/autolayout/libsbml_ne_fruchterman_reingold_algorithm.cpp
+++ src/autolayout/libsbml_ne_fruchterman_reingold_algorithm.cpp
@@ -5,6 +5,10 @@
 #include <cstdlib>
 #include <cmath>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 namespace LIBSBML_NETWORKEDITOR_CPP_NAMESPACE {
 
 FruthtermanReingoldAlgorithm::FruthtermanReingoldAlgorithm() {
