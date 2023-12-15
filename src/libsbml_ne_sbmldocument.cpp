8b5f921f2c482c9ee07a1fd271903b44086a0a53
adelhpour <>
2023-12-15 09:30:06 -0800

    making the sbmldocument layer functions C-compatible

--- src/libsbml_ne_sbmldocument.cpp
+++ src/libsbml_ne_sbmldocument.cpp
@@ -2,157 +2,82 @@
 #include "libsbml_ne_sbmldocument_layout.h"
 #include "libsbml_ne_sbmldocument_render.h"
 
-namespace LIBSBML_NETWORKEDITOR_CPP_NAMESPACE  {
+extern "C" {
 
-SBMLDocument* readSBML(const std::string& sbml) {
-    SBMLDocument* document = readSBMLFromFile(sbml.c_str());
-    if (document && document->isSetModel())
-        return document;
-    document = readSBMLFromString(sbml.c_str());
-    if (document && document->isSetModel())
-        return document;
+namespace LIBSBML_NETWORKEDITOR_CPP_NAMESPACE {
 
-    return NULL;
-}
+    int autolayoutWithDefaultParameters(SBMLDocument* document) {
+        if (!createDefaultLayout(document, 10.0, 15.0, false, false, false) && !createDefaultRenderInformation(document))
+            return 0;
 
-bool writeSBML(SBMLDocument* document, const std::string &fileName) {
-    return writeSBMLToFile(document, fileName.c_str());
-}
-
-const std::string writeSBML(SBMLDocument* document) {
-    return writeSBMLToString(document);
-}
-
-int autolayout(SBMLDocument* document, const double& stiffness, const double& gravity, const bool& useMagnetism, const bool& useBoundary, const bool& useGrid) {
-    if (!createDefaultLayout(document, stiffness, gravity, useMagnetism, useBoundary, useGrid) && !createDefaultRenderInformation(document))
-        return 0;
-
-    return -1;
-}
-
-bool isSetId(SBase* object) {
-    if (object)
-        return object->isSetId();
-
-    return false;
-}
-
-const std::string getId(SBase* object) {
-    if (object)
-        return object->getId();
-
-    return "";
-}
-
-int setId(SBase* object, const std::string& sid) {
-    if (object) {
-        object->setId(sid);
-        return 0;
+        return -1;
     }
 
-    return -1;
-}
+    int autolayout(SBMLDocument* document, const double stiffness, const double gravity, const bool useMagnetism,
+                   const bool useBoundary, const bool useGrid) {
+        if (!createDefaultLayout(document, stiffness, gravity, useMagnetism, useBoundary, useGrid) && !createDefaultRenderInformation(document))
+            return 0;
 
-bool isSetName(SBase* object) {
-    if (object)
-        object->isSetName();
-
-    return false;
-}
-
-const std::string getName(SBase* object) {
-    if (object)
-        return object->getName();
-
-    return "";
-}
-
-int setName(SBase* object, const std::string& name) {
-    if (object) {
-        object->setName(name);
-        return 0;
+        return -1;
     }
 
-    return -1;
-}
+    Compartment *getCompartment(SBMLDocument *document, const char* id) {
+        if (document->isSetModel())
+            return document->getModel()->getCompartment(id);
 
-bool isSetMetaId(SBase* object) {
-    if (object)
-        return object->isSetMetaId();
-
-    return false;
-}
-
-const std::string getMetaId(SBase* object) {
-    if (object)
-        return object->getMetaId();
-
-    return "";
-}
-
-int setMetaId(SBase* object, const std::string& metaid) {
-    if (object) {
-        object->setMetaId(metaid);
-        return 0;
+        return NULL;
     }
 
-    return -1;
-}
+    Species *getSpecies(SBMLDocument *document, const char* id) {
+        if (document->isSetModel())
+            return document->getModel()->getSpecies(id);
 
-Compartment* getCompartment(SBMLDocument* document, const std::string& id) {
-    if (document->isSetModel())
-        return document->getModel()->getCompartment(id);
+        return NULL;
+    }
 
-    return NULL;
-}
+    Reaction *getReaction(SBMLDocument *document, const char* id) {
+        if (document->isSetModel())
+            return document->getModel()->getReaction(id);
 
-Species* getSpecies(SBMLDocument* document, const std::string& id) {
-    if (document->isSetModel())
-        return document->getModel()->getSpecies(id);
+        return NULL;
+    }
 
-    return NULL;
-}
-
-Reaction* getReaction(SBMLDocument* document, const std::string& id) {
-    if (document->isSetModel())
-        return document->getModel()->getReaction(id);
-
-    return NULL;
-}
-
-SpeciesReference* getSpeciesReference(SBMLDocument* document, const std::string& reactionId, const std::string& speciesId) {
-    if (document->isSetModel()) {
-        Reaction* reaction = getReaction(document, reactionId);
-        if (reaction) {
-            if (reaction->getReactant(speciesId))
-                return reaction->getReactant(speciesId);
-            else if (reaction->getProduct(speciesId))
-                return reaction->getProduct(speciesId);
+    SpeciesReference *
+    getSpeciesReference(SBMLDocument *document, const char* reactionId, const char* speciesId) {
+        if (document->isSetModel()) {
+            Reaction *reaction = getReaction(document, reactionId);
+            if (reaction) {
+                if (reaction->getReactant(speciesId))
+                    return reaction->getReactant(speciesId);
+                else if (reaction->getProduct(speciesId))
+                    return reaction->getProduct(speciesId);
+            }
         }
+
+        return NULL;
     }
 
-    return NULL;
-}
+    SpeciesReference *
+    getReactantSpeciesReference(SBMLDocument *document, const char* reactionId, const char* speciesId) {
+        if (document->isSetModel()) {
+            Reaction *reaction = getReaction(document, reactionId);
+            if (reaction)
+                reaction->getReactant(speciesId);
+        }
 
-SpeciesReference* getReactantSpeciesReference(SBMLDocument* document, const std::string& reactionId, const std::string& speciesId) {
-    if (document->isSetModel()) {
-        Reaction* reaction = getReaction(document, reactionId);
-        if (reaction)
-            reaction->getReactant(speciesId);
+        return NULL;
     }
 
-    return NULL;
-}
+    ModifierSpeciesReference *
+    getModifierSpeciesReference(SBMLDocument *document, const char* reactionId, const char* speciesId) {
+        if (document->isSetModel()) {
+            Reaction *reaction = getReaction(document, reactionId);
+            if (reaction)
+                reaction->getModifier(speciesId);
+        }
 
-ModifierSpeciesReference* getModifierSpeciesReference(SBMLDocument* document, const std::string& reactionId, const std::string& speciesId) {
-    if (document->isSetModel()) {
-        Reaction* reaction = getReaction(document, reactionId);
-        if (reaction)
-            reaction->getModifier(speciesId);
+        return NULL;
     }
-
-    return NULL;
 }
-
 
 }
