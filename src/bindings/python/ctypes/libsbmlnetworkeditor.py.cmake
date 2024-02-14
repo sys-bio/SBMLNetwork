25779d137e44b30fef02b935090c6d6a2aadd8e2
adelhpour <ad.heydarabadi@gmail.com>
2024-02-14 09:51:18 -0800

    c_api function are added to the python-ctypes layer

--- src/bindings/python/ctypes/libsbmlnetworkeditor.py.cmake
+++ src/bindings/python/ctypes/libsbmlnetworkeditor.py.cmake
@@ -15,7 +15,6 @@ class SBMLDocument(ctypes.Structure):
     pass
 
 lib = ctypes.CDLL(lib_path)
-lib.readSBML.restype = ctypes.POINTER(SBMLDocument)
 
 class LibSBMLNetworkEditor:
     """
@@ -46,8 +45,8 @@ class LibSBMLNetworkEditor:
 
             SBMLDocument: a pointer to the SBMLDocument structure created from the SBML content in the given file name or from the SBML content in the given text string
         """
-
-        self.sbml_object = lib.readSBML(str(sbml).encode())
+        lib.c_api_readSBML.restype = ctypes.POINTER(SBMLDocument)
+        self.sbml_object = lib.c_api_readSBML(str(sbml).encode())
 
     def export(self, file_name=""):
         """
@@ -64,9 +63,11 @@ class LibSBMLNetworkEditor:
         """
         
         if file_name:
-            return lib.writeSBML(self.sbml_object, str(file_name).encode())
+            lib.c_api_writeSBMLToFile.restype = ctypes.c_int
+            return lib.c_api_writeSBMLToFile(self.sbml_object, str(file_name).encode())
         else:
-            return lib.writeSBMLToString(self.sbml_object).decode()
+            lib.c_api_writeSBMLToString.restype = ctypes.c_char_p
+            return ctypes.c_char_p(lib.c_api_writeSBMLToString(self.sbml_object)).value.decode()
         
     def autolayout(self, stiffness=10, gravity=15, use_magnetism=False, use_boundary=False, use_grid=False, locked_nodes=[]):
         """
@@ -92,4 +93,317 @@ class LibSBMLNetworkEditor:
             for i in range(len(locked_nodes)):
                 locked_nodes_ptr[i] = ctypes.c_char_p(locked_nodes[i].encode())
 
-        return lib.autolayout(self.sbml_object, ctypes.c_double(stiffness), ctypes.c_double(gravity), use_magnetism, use_boundary, use_grid, locked_nodes_ptr)
+        return lib.c_api_autolayout(self.sbml_object, ctypes.c_double(stiffness), ctypes.c_double(gravity), use_magnetism, use_boundary, use_grid, locked_nodes_ptr)
+
+    def getNumLayouts(self):
+        """
+        Returns the number of Layout objects in the given SBMLDocument
+
+        :Returns:
+
+            an integer that determines the number of Layout objects in the given SBMLDocument
+        """
+        return lib.c_api_getNumLayouts(self.sbml_object)
+
+    def removeLayouts(self):
+        """
+        Removes all the Layout objects from the given SBMLDocument
+
+        :Returns:
+
+            true on success and false if the Layout objects could not be removed
+        """
+        return lib.c_api_removeAllLayouts(self.sbml_object)
+
+    def getWidth(self, layout_index=0):
+        """
+        Returns the width of the canvas of the Layout object with the given index in the given SBMLDocument
+
+        :Parameters:
+
+            - layout_index (int, optional): an integer (default: 0) that determines the index of the Layout object in the given SBMLDocument
+
+        :Returns:
+
+            a float that determines the width of the canvas of the the Layout object with the given index in the given SBMLDocument
+        """
+        lib.c_api_getWidth.restype = ctypes.c_double
+        return lib.c_api_getWidth(self.sbml_object, layout_index)
+
+    def setWidth(self, width, layout_index=0):
+        """
+        Sets the width of the canvas of the Layout object with the given index in the given SBMLDocument
+
+        :Parameters:
+
+            - width (float): a float that determines the width of the canvas of the Layout object with the given index in the given SBMLDocument
+            - layout_index (int, optional): an integer (default: 0) that determines the index of the Layout object in the given SBMLDocument
+
+        :Returns:
+
+            true on success and false if the width of the canvas could not be set
+        """
+        return lib.c_api_setWidth(self.sbml_object, ctypes.c_double(width), layout_index)
+
+    def getHeight(self, layout_index=0):
+        """
+        Returns the height of the canvas of the Layout object with the given index in the given SBMLDocument
+
+        :Parameters:
+
+            - layout_index (int, optional): an integer (default: 0) that determines the index of the Layout object in the given SBMLDocument
+
+        :Returns:
+
+            a float that determines the height of the canvas of the Layout object with the given index in the given SBMLDocument
+        """
+        lib.c_api_getHeight.restype = ctypes.c_double
+        return lib.c_api_getHeight(self.sbml_object, layout_index)
+
+    def setHeight(self, height, layout_index=0):
+        """
+
+        Sets the height of the canvas of the Layout object with the given index in the given SBMLDocument
+
+        :Parameters:
+
+            - height (float): a float that determines the height of the canvas of the Layout object with the given index in the given SBMLDocument
+            - layout_index (int, optional): an integer (default: 0) that determines the index of the Layout object in the given SBMLDocument
+
+        :Returns:
+
+            true on success and false if the height of the canvas could not be set
+        """
+        return lib.c_api_setHeight(self.sbml_object, ctypes.c_double(height), layout_index)
+
+    def getNumAllGraphicalObjects(self, layout_index=0):
+        """
+        Returns the number of GraphicalObjects in the the Layout object with the given index in the given SBMLDocument
+
+        :Parameters:
+
+            - layout_index (int, optional): an integer (default: 0) that determines the index of the Layout object in the given SBMLDocument
+
+        :Returns:
+
+            an integer that determines the number of GraphicalObjects in the the Layout object with the given index in the given SBMLDocument
+        """
+        return lib.c_api_getNumAllGraphicalObjects(self.sbml_object, layout_index)
+
+    def getNumGraphicalObjects(self, id, layout_index=0):
+        """
+        Returns the number of GraphicalObjects associated with the given id in the Layout object with the given index in the given SBMLDocument
+
+        :Parameters:
+
+            - id (string): a string that determines the id of the GraphicalObject
+            - layout_index (int, optional): an integer (default: 0) that determines the index of the Layout object in the given SBMLDocument
+
+        :Returns:
+
+            an integer that determines the number of GraphicalObjects associated with the given id in the Layout object with the given index in the given SBMLDocument
+            """
+        return lib.c_api_getNumGraphicalObjects(self.sbml_object, str(id).encode(), layout_index)
+
+    def getNumAllCompartmentGlyphs(self, layout_index=0):
+        """
+        Returns the number of CompartmentGlyphs in the Layout object with the given index in the given SBMLDocument
+
+        :Parameters:
+
+            - layout_index (int, optional): an integer (default: 0) that determines the index of the Layout object in the given SBMLDocument
+
+        :Returns:
+
+            an integer that determines the number of CompartmentGlyphs in the Layout object with the given index in the given SBMLDocument
+        """
+        return lib.c_api_getNumAllCompartmentGlyphs(self.sbml_object, layout_index)
+
+    def getNumCompartmentGlyphs(self, compartment_id, layout_index=0):
+        """
+        Returns the number of CompartmentGlyphs associated with the given compartment_id in the Layout object with the given index in the given SBMLDocument
+
+        :Parameters:
+
+            - compartment_id (string): a string that determines the id of the Compartment
+            - layout_index (int, optional): an integer (default: 0) that determines the index of the Layout object in the given SBMLDocument
+
+        :Returns:
+
+                an integer that determines the number of CompartmentGlyphs associated with the given compartment_id in the Layout object with the given index in the given SBMLDocument
+            """
+        return lib.c_api_getNumCompartmentGlyphs(self.sbml_object, str(compartment_id).encode(), layout_index)
+
+    def isCompartmentGlyph(self, compartment_id, layout_index=0):
+        """
+        Returns whether the given compartment_id is associated with a CompartmentGlyph in the Layout object with the given index in the given SBMLDocument
+
+        :Parameters:
+
+            - compartment_id (string): a string that determines the id of the Compartment
+            - layout_index (int, optional): an integer (default: 0) that determines the index of the Layout object in the given SBMLDocument
+
+        :Returns:
+
+            true if the given compartment_id is associated with a CompartmentGlyph in the Layout object with the given index in the given SBMLDocument and false otherwise
+            """
+        return lib.c_api_isCompartmentGlyph(self.sbml_object, str(compartment_id).encode(), layout_index)
+
+    def getNumAllSpeciesGlyphs(self, layout_index=0):
+        """
+        Returns the number of SpeciesGlyphs in the Layout object with the given index in the given SBMLDocument
+
+        :Parameters:
+
+            - layout_index (int, optional): an integer (default: 0) that determines the index of the Layout object in the given SBMLDocument
+
+        :Returns:
+
+            an integer that determines the number of SpeciesGlyphs in the Layout object with the given index in the given SBMLDocument
+        """
+        return lib.c_api_getNumAllSpeciesGlyphs(self.sbml_object, layout_index)
+
+    def getNumSpeciesGlyphs(self, species_id, layout_index=0):
+        """
+        Returns the number of SpeciesGlyphs associated with the given species_id in the Layout object with the given index in the given SBMLDocument
+
+        :Parameters:
+
+            - species_id (string): a string that determines the id of the Species
+            - layout_index (int, optional): an integer (default: 0) that determines the index of the Layout object in the given SBMLDocument
+
+        :Returns:
+
+            an integer that determines the number of SpeciesGlyphs associated with the given species_id in the Layout object with the given index in the given SBMLDocument
+        """
+        return lib.c_api_getNumSpeciesGlyphs(self.sbml_object, str(species_id).encode(), layout_index)
+
+    def isSpeciesGlyph(self, species_id, layout_index=0):
+        """
+        Returns whether the given species_id is associated with a SpeciesGlyph in the Layout object with the given index in the given SBMLDocument
+
+        :Parameters:
+
+            - species_id (string): a string that determines the id of the Species
+            - layout_index (int, optional): an integer (default: 0) that determines the index of the Layout object in the given SBMLDocument
+
+        :Returns:
+
+            true if the given species_id is associated with a SpeciesGlyph in the Layout object with the given index in the given SBMLDocument and false otherwise
+            """
+        return lib.c_api_isSpeciesGlyph(self.sbml_object, str(species_id).encode(), layout_index)
+
+    def getNumAllReactionGlyphs(self, layout_index=0):
+        """
+        Returns the number of ReactionGlyphs in the Layout object with the given index in the given SBMLDocument
+
+        :Parameters:
+
+            - layout_index (int, optional): an integer (default: 0) that determines the index of the Layout object in the given SBMLDocument
+
+        :Returns:
+
+            an integer that determines the number of ReactionGlyphs in the Layout object with the given index in the given SBMLDocument
+            """
+        return lib.c_api_getNumAllReactionGlyphs(self.sbml_object, layout_index)
+
+    def getNumReactionGlyphs(self, reaction_id, layout_index=0):
+        """
+        Returns the number of ReactionGlyphs associated with the given reaction_id in the Layout object with the given index in the given SBMLDocument
+
+        :Parameters:
+
+            - reaction_id (string): a string that determines the id of the Reaction
+            - layout_index (int, optional): an integer (default: 0) that determines the index of the Layout object in the given SBMLDocument
+
+        :Returns:
+
+            an integer that determines the number of ReactionGlyphs associated with the given reaction_id in the Layout object with the given index in the given SBMLDocument
+            """
+
+        return lib.c_api_getNumReactionGlyphs(self.sbml_object, str(reaction_id).encode(), layout_index)
+
+    def isReactionGlyph(self, reaction_id, layout_index=0):
+        """
+        Returns whether the given reaction_id is associated with a ReactionGlyph in the Layout object with the given index in the given SBMLDocument
+
+        :Parameters:
+
+            - reaction_id (string): a string that determines the id of the Reaction
+            - layout_index (int, optional): an integer (default: 0) that determines the index of the Layout object in the given SBMLDocument
+
+        :Returns:
+
+            true if the given reaction_id is associated with a ReactionGlyph in the Layout object with the given index in the given SBMLDocument and false otherwise
+            """
+        return lib.c_api_isReactionGlyph(self.sbml_object, str(reaction_id).encode(), layout_index)
+
+    def getNumSpeciesReferenceGlyphs(self, reaction_id, reaction_glyph_index=0, layout_index=0):
+        """
+        Returns the number of SpeciesReferenceGlyphs associated with the given reaction_id and reaction_glyph_index in the Layout object with the given index in the given SBMLDocument
+
+        :Parameters:
+
+            - reaction_id (string): a string that determines the id of the Reaction
+            - reaction_glyph_index (int): an integer that determines the index of the ReactionGlyph in the given SBMLDocument
+            - layout_index (int, optional): an integer (default: 0) that determines the index of the Layout object in the given SBMLDocument
+
+        :Returns:
+
+            an integer that determines the number of SpeciesReferenceGlyphs associated with the given reaction_id and reaction_glyph_index in the Layout object with the given index in the given SBMLDocument
+            """
+        return lib.c_api_getNumSpeciesReferenceGlyphs(self.sbml_object, str(reaction_id).encode(), reaction_glyph_index, layout_index)
+
+    def isSetRole(self, reaction_id, reaction_glyph_index=0, species_reference_glyph_index=0, layout_index=0):
+        """
+        Returns whether the role of the SpeciesReferenceGlyph with the given reaction_id, reaction_glyph_index, species_reference_glyph_index, and layout_index in the given SBMLDocument is set
+
+        :Parameters:
+
+            - reaction_id (string): a string that determines the id of the Reaction
+            - reaction_glyph_index (int): an integer that determines the index of the ReactionGlyph in the given SBMLDocument
+            - species_reference_glyph_index (int): an integer that determines the index of the SpeciesReferenceGlyph in the given SBMLDocument
+            - layout_index (int, optional): an integer (default: 0) that determines the index of the Layout object in the given SBMLDocument
+
+        :Returns:
+
+            true if the role of the SpeciesReferenceGlyph with the given reaction_id, reaction_glyph_index, species_reference_glyph_index, and layout_index in the given SBMLDocument is set and false otherwise
+            """
+
+        return lib.c_api_isSetRole(self.sbml_object, str(reaction_id).encode(), reaction_glyph_index, species_reference_glyph_index, layout_index)
+
+    def getRole(self, reaction_id, reaction_glyph_index=0, species_reference_glyph_index=0, layout_index=0):
+        """
+        Returns the role of the SpeciesReferenceGlyph with the given reaction_id, reaction_glyph_index, species_reference_glyph_index, and layout_index in the given SBMLDocument
+
+        :Parameters:
+
+            - reaction_id (string): a string that determines the id of the Reaction
+            - reaction_glyph_index (int): an integer that determines the index of the ReactionGlyph in the given SBMLDocument
+            - species_reference_glyph_index (int): an integer that determines the index of the SpeciesReferenceGlyph in the given SBMLDocument
+            - layout_index (int, optional): an integer (default: 0) that determines the index of the Layout object in the given SBMLDocument
+
+        :Returns:
+
+            a string that determines the role of the SpeciesReferenceGlyph with the given reaction_id, reaction_glyph_index, species_reference_glyph_index, and layout_index in the given SBMLDocument
+        """
+        lib.c_api_getRole.restype = ctypes.c_char_p
+        return ctypes.c_char_p(lib.c_api_getRole(self.sbml_object, str(reaction_id).encode(), reaction_glyph_index, species_reference_glyph_index, layout_index)).value.decode()
+
+    def setRole(self, reaction_id, role, reaction_glyph_index=0, species_reference_glyph_index=0, layout_index=0):
+        """
+        Sets the role of the SpeciesReferenceGlyph with the given reaction_id, reaction_glyph_index, species_reference_glyph_index, and layout_index in the given SBMLDocument
+
+        :Parameters:
+
+            - role (string): a string that determines the role of the SpeciesReferenceGlyph
+            - reaction_id (string): a string that determines the id of the Reaction
+            - reaction_glyph_index (int): an integer that determines the index of the ReactionGlyph in the given SBMLDocument
+            - species_reference_glyph_index (int): an integer that determines the index of the SpeciesReferenceGlyph in the given SBMLDocument
+            - layout_index (int, optional): an integer (default: 0) that determines the index of the Layout object in the given SBMLDocument
+
+        :Returns:
+
+            true on success and false if the role of the SpeciesReferenceGlyph could not be set
+            """
+        return lib.c_api_setRole(self.sbml_object, str(reaction_id).encode(), str(role).encode(), reaction_glyph_index, species_reference_glyph_index, layout_index)
