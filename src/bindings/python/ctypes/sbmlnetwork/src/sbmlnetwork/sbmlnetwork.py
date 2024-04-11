3cf436644e4983857f11f52563453bfa57658e3c
adelhpour <ad.heydarabadi@gmail.com>
2024-04-11 14:54:20 -0700

    SBMLNetwork repo is added as a sub-directory to libSBMLNetwork

---
+++ src/bindings/python/ctypes/sbmlnetwork/src/sbmlnetwork/sbmlnetwork.py
@@ -0,0 +1,35 @@
+import libsbmlnetwork
+import networkinfotranslator
+from IPython.display import display
+
+
+class SBMLNetwork(libsbmlnetwork.LibSBMLNetwork):
+
+    def __init__(self, sbml=""):
+        super().__init__(sbml)
+
+    def draw(self, file_name=""):
+        """
+        Draws the network of the SBML model. Saves the figure to the file_directory if specified, otherwise displays the figure.
+
+        :param file_directory:
+        :param file_name:
+        :param file_format:
+        """
+        if file_name:
+            networkinfotranslator.import_sbml_export_figure(self.save(), file_name, self.use_name_as_text_label, self.display_reactions_text_label)
+        else:
+            display(networkinfotranslator.import_sbml_export_pil_image(self.save(), self.use_name_as_text_label, self.display_reactions_text_label))
+
+
+def load(sbml):
+    """
+    Loads the SBML model.
+
+    :param sbml: The SBML model.
+    :return: SBMLNetwork
+    """
+    return SBMLNetwork(sbml)
+
+
+
