8f99f42129ebe0b5418d109abbe51de2c6ecd764
adelhpour <ad.heydarabadi@gmail.com>
2024-04-13 21:10:44 -0700

    sbmlnetwork test setUp function is updated to find the sbml models

--- src/bindings/python/ctypes/sbmlnetwork/tests/test_sbml_network.py
+++ src/bindings/python/ctypes/sbmlnetwork/tests/test_sbml_network.py
@@ -12,10 +12,10 @@ class TestSBMLNetwork(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.networks = []
-        files = os.listdir(os.getcwd() + '/models')
+        files = os.listdir(os.path.realpath(__file__) + '/models')
         for file in files:
             if file.endswith('.xml'):
-                cls.networks.append(sbmlnetwork.load(os.getcwd() + '/models/' + file))
+                cls.networks.append(sbmlnetwork.load(os.path.realpath(__file__) + '/models/' + file))
 
     def test_layout_render_info(self):
         for network in self.networks:
