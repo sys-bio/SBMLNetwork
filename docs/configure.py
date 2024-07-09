1fe094dd1a022b87961482b38c934f032df3b158
adelhpour <ad.heydarabadi@gmail.com>
2024-07-08 17:59:55 -0700

    docs are configured

--- docs/configure.py
+++ docs/configure.py
@@ -1,6 +1,7 @@
 # docs/configure.py
 
 import shutil
+import os
 
 def read_version_from_file(version_file):
     with open(version_file, 'r') as f:
@@ -8,6 +9,8 @@ def read_version_from_file(version_file):
     return version
 
 def replace_placeholder_in_file(input_file, output_file, placeholder, replacement):
+    # print current working directory
+    print(f"Current working directory: {os.getcwd()}")
     with open(input_file, 'r') as fin:
         content = fin.read()
 
