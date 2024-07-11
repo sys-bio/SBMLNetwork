e54351854bf2079a04b0760ad3abfef2ff81dbc0
adelhpour <ad.heydarabadi@gmail.com>
2024-07-10 17:36:58 -0700

    docs are updated

--- docs/api_reference/python_api_index.rst
+++ docs/api_reference/python_api_index.rst
@@ -2,3 +2,8 @@ Python API Reference
 ====================
 
 This is the API reference for the Python client library for the SBMLNetwork package.
+
+.. automodule:: libsbmlnetwork
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- docs/pre_conf.py
+++ docs/pre_conf.py
@@ -16,8 +16,22 @@ def replace_placeholder_in_file(input_file, output_file, placeholder, replacemen
 
 if __name__ == "__main__":
     version_file = "../VERSION.txt"
+    version = read_version_from_file(version_file)
+    # libsbmlnetwork
+    input_file = "../src/bindings/python/ctypes/libsbmlnetwork.py.cmake"
+    output_file = "./libsbmlnetwork.py"
+    placeholder = "${LIBSBMLNETWORK_DOTTED_VERSION}"
+    replace_placeholder_in_file(input_file, output_file, placeholder, version)
+
+    # Doxyfile
     input_file = "Doxyfile.in"
     output_file = "Doxyfile"
     placeholder = "@LIBSBMLNETWORK_DOTTED_VERSION@"
-    version = read_version_from_file(version_file)
     replace_placeholder_in_file(input_file, output_file, placeholder, version)
+
+    # conf.py
+    input_file = "conf.py.in"
+    output_file = "conf.py"
+    placeholder = "@LIBSBMLNETWORK_DOTTED_VERSION@"
+    replace_placeholder_in_file(input_file, output_file, placeholder, version)
+
