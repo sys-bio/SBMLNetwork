5e8316473cbad1251425ff8ea767706a2b7a2975
adelhpour <ad.heydarabadi@gmail.com>
2025-02-07 11:52:52 -0800

    make changes to see if the alias reaction test passes on windows

--- src/features/alias_elements/libsbmlnetwork_alias_reaction.cpp
+++ src/features/alias_elements/libsbmlnetwork_alias_reaction.cpp
@@ -52,10 +52,9 @@ namespace LIBSBMLNETWORK_CPP_NAMESPACE {
                             speciesGlyphAliasSpeciesGlyphIds[speciesGlyph->getId()]);
                 if (connectedSpeciesGlyph != NULL) {
                     speciesGlyphAliasSpeciesGlyphIds[speciesGlyph->getId()] = connectedSpeciesGlyph->getId();
-
+                    std::cout << "";
                     int stoichiometry = getStoichiometryAsInteger(layout,
                             findSpeciesReference(document->getModel(), layout, referenceReactionGlyph, speciesGlyph));
-                    std::cout << "";
                     for (unsigned int stoichiometryIndex = 0; stoichiometryIndex < stoichiometry; stoichiometryIndex++)
                         alias_element_createAliasSpeciesReferenceGlyph(reactionGlyph, speciesReferenceGlyph,
                                                                        connectedSpeciesGlyph->getId(),
