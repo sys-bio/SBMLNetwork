43e5267b4ed99856e951617ef607d84dca634d8d
adelhpour <ad.heydarabadi@gmail.com>
2025-02-07 10:28:21 -0800

    The feature to add a color bar is added

--- src/bindings/python/ctypes/sbmlnetwork/src/sbmlnetwork/network_elements/__init__.py
+++ src/bindings/python/ctypes/sbmlnetwork/src/sbmlnetwork/network_elements/__init__.py
@@ -2,4 +2,5 @@ from .compartment import Compartment
 from .species import Species
 from .reaction import Reaction
 from .additional_element import AdditionalElement
+from .color_bar import ColorBar
 from .network_element_lists import *
